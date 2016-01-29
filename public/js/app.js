1b7c533cef3b568d84667eaeed6e93583881e645
Andrew Favaloro <andrew.favaloro@gmail.com>
2016-01-29 16:12:27 +0000

    Consolidating JS / HTML files

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
-// public/js/app.js
-angular.module('sampleApp', ['ngRoute', 
-                             'appRoutes', 
-                             'MainCtrl',
-                             'StatsCtrl',
-                             'LoginCtrl',
-                             'AskedCtrl',
-                             'ProfileCtrl',
-                             'AddQuestionCtrl'                             
-                            ]);
+// // public/js/app.js
+// angular.module('sampleApp', ['ngRoute', 
+//                              'appRoutes', 
+//                              'MainCtrl',
+//                              'StatsCtrl',
+//                              'LoginCtrl',
+//                              'AskedCtrl',
+//                              'ProfileCtrl',
+//                              'AddQuestionCtrl'                             
+//                             ]);
--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,33 +1,33 @@
 // public/js/appRoutes.js QSP
-angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginController'
-        }).when('/login', {
-            templateUrl: 'views/login.html',
-            controller: 'LoginController'
-        }).when('/stats',{
-            templateUrl: 'views/stats.html',
-            controller: 'StatsController'
-        }).when('/home',{
-            templateUrl: 'views/home.html',
-            controller: 'MainController'
-        }).when('/addQuestion',{
-            templateUrl: 'views/addQuestion.html',
-            controller: 'AddQuestionController'
-        }).when('/addUser',{
-            templateUrl: 'views/addUser.html',
-            controller: 'LoginController'
-        }).when('/askedQuestions',{
-            templateUrl: 'views/askedQuestions.html',
-            controller: 'AskedController'
-        }).when('/answeredQuestions',{
-            templateUrl: 'views/askedQuestions.html',
-            controller: 'AskedController'
-        }).when('/profile',{
-            templateUrl: 'views/profile.html',
-            controller: 'ProfileController'
-        });
-    $locationProvider.html5Mode(true);
-}]);
+// angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: 'views/login.html',
+//             controller: 'LoginController'
+//         }).when('/login', {
+//             templateUrl: 'views/login.html',
+//             controller: 'LoginController'
+//         }).when('/stats',{
+//             templateUrl: 'views/stats.html',
+//             controller: 'StatsController'
+//         }).when('/home',{
+//             templateUrl: 'views/home.html',
+//             controller: 'MainController'
+//         }).when('/addQuestion',{
+//             templateUrl: 'views/addQuestion.html',
+//             controller: 'AddQuestionController'
+//         }).when('/addUser',{
+//             templateUrl: 'views/addUser.html',
+//             controller: 'LoginController'
+//         }).when('/askedQuestions',{
+//             templateUrl: 'views/askedQuestions.html',
+//             controller: 'AskedController'
+//         }).when('/answeredQuestions',{
+//             templateUrl: 'views/askedQuestions.html',
+//             controller: 'AskedController'
+//         }).when('/profile',{
+//             templateUrl: 'views/profile.html',
+//             controller: 'ProfileController'
+//         });
+//     $locationProvider.html5Mode(true);
+// }]);
