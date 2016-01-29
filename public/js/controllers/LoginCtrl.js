1ca2e7b932afb7d6e3eec795a5414871281357e1
Andrew Favaloro <andrew.favaloro@gmail.com>
2016-01-29 17:12:12 +0000

    Rendering button 2

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,6 +1,6 @@
 qsp.controller('LoginController', function($scope,$http,$location,$rootScope) {
   
-  gapi.signin2.render("signinwithgoogle",{
+  /*gapi.signin2.render("signinwithgoogle",{
     'scope': 'email',
     'width': 200,
     'height': 50,
@@ -8,7 +8,7 @@ qsp.controller('LoginController', function($scope,$http,$location,$rootScope) {
     'theme': 'dark',
     'onsuccess': handleSuccess,
     'onfailure': handleFailure
-  });
+  });*/
   
   $scope.errmsg    = "";
   $scope.userID    = $rootScope.userID ? $rootScope.userID : "";
