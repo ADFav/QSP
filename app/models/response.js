9c0a09eb980748419a3f9b4cabc2baae724e380b
Andrew Favaloro <andrew.favaloro@gmail.com>
2016-02-02 20:43:48 +0000

    re-adding bootstrap

--- app/models/response.js
+++ app/models/response.js
@@ -3,9 +3,6 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// define our nerd model
-// module.exports allows us to pass this to other files when it is called
-
 var responseSchema = new Schema({
   timeStamp  : {type : Date,    required : true},
   userid     : {type : Number,  required : true},
@@ -13,10 +10,8 @@ var responseSchema = new Schema({
   user_age   : {type : Number,  required : true},
   user_state : {type : String,  required : true},
   gender     : {type : String,  required : true},
-  response   : {type : Boolean, required : true}
+  response   : {type : Boolean}
 });
 
 var Response = mongoose.model('Response', responseSchema);
-
-// make this available to our users in our Node applications
 module.exports = Response;
--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,56 +1,36 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope,$http,$location,$rootScope) {
-  
   if(!$rootScope.userID) {$location.path('/login');}
   
-  $scope.goProfile = function(uid){
-    $location.path('/profile').search({uid:uid})
-  }
-  
-//   $scope.qid        = 1;
-//   $scope.asker      = "";
-//   $scope.question   = "";
-//   $scope.response_1 = "";
-//   $scope.response_2 = "";  
-//   $scope.errmsg     = "";
-  
+  $scope.goProfile = function(uid){$location.path('/profile').search({uid:uid});};
+   
   $scope.switchQuestion = function(){
     $http.get('/getNewQuestion?uid='+$rootScope.userID).success(function(q_obj){
       if(!q_obj.errmsg){
-        $scope.errmsg     = "";
-        $scope.question   = q_obj.text;
-        $scope.response_1 = q_obj.responses[0];
-        $scope.response_2 = q_obj.responses[1];
-        $scope.qid        = q_obj._id;
-        $scope.askerid    = q_obj.asker;
+        $scope.question_object = q_obj;
         $http.get('/getUser?uid='+q_obj.asker).success(function(user){
-          $scope.asker      = user.username;
+          $scope.asker = user.username;
         });
       }
       else{
-        $scope.errmsg     = q_obj.errmsg;
-        $scope.asker      = "";
-        $scope.question   = "";
-        $scope.response_1 = "";
-        $scope.response_2 = "";
-        $scope.qid        = "";
-        $scope.askerid    = "";
+       $scope.question_object = {"_id" : -1, "asker" : -1, "text": "", "responses" : ["", ""], "errmsg": q_obj.errmsg};
+        $scope.asker = "";
       }
     });
   };  
   
+  $scope.question_object = {"_id" : -1, "asker" : -1, "text": "", "responses" : ["", ""], "errmsg": ""};
   $scope.switchQuestion()
   
-  $scope.postAns   = function(resp){
+  $scope.postAns = function(resp){
     if(!$scope.errmsg){
-      var postData = {uid:$rootScope.userID, qid:$scope.qid, response:resp};
-      $http.post("/newResponse",postData).
-        success(function(data){
+      var postData = {uid:$rootScope.userID, qid:$scope.question_object._id, response:resp};
+      $http.post("/newResponse",postData).success(function(data){
           $scope.switchQuestion();    
-      }).error (function(data){
-      });
+      }).error (function(data){});
     }
   };
   $scope.postTrue  = function(){$scope.postAns(true); };
   $scope.postFalse = function(){$scope.postAns(false);};
+  $scope.abstain   = function(){$scope.postAns();     };
 });
